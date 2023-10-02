5480694249b3e4a5260de938b1ecd6f87fd61928
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-10-02 22:48:36 +0530

    committed

---
+++ CalculatorWebServicesDemo/CalculatorWebApplication/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorWebApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnAdd_Click1(object sender, EventArgs e)
+        {
+            CalculatorService.CalculatorWebServiceSoapClient client = 
+                new CalculatorService.CalculatorWebServiceSoapClient("CalculatorWebServiceSoap");
+            int result = client.Add(Convert.ToInt32(txtFirstNumber.Text),
+                                    Convert.ToInt32(txtFirstNumber.Text));
+            lblResult.Text = result.ToString();
+            //System.Console.WriteLine("Hello");
+        }
+    }
+}
